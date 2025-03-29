91d678d11bd8b8e191cfa9f73b85ed00b071de96
Pato Marques <patoxmarques@gmail.com>
2025-03-29 01:14:39 -0300

    fix: arrows has changed

--- assets/js/wp-laroye.js
+++ assets/js/wp-laroye.js
@@ -1,31 +1,35 @@
 jQuery(document).ready(function ($) {
-    $.noConflict();
-    console.log("jQuery is still working!", $);
+  $.noConflict();
+  console.log("jQuery is still working!", $);
 
-    $('.slick-js').slick({
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 4,
-        adaptiveHeight: true,
-        autoplay: true,
-        centerMode: false,   
-    });
+  $(".slick-js").slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    adaptiveHeight: true,
+    autoplay: true,
+    centerMode: false,
+  });
 
-     $('.slick-people').slick({
-        // dots: false,
-        // infinite: true,
-        // speed: 300,
-        slidesToShow: 4,
-        // adaptiveHeight: true,
-        // autoplay: true,
-        // centerMode: false,   
-        arrows: true
-    });
+  $(".slick-people").slick({
+    // dots: false,
+    // infinite: true,
+    // speed: 300,
+    slidesToShow: 4,
+    // adaptiveHeight: true,
+    // autoplay: true,
+    // centerMode: false,
+    arrows: true,
+    prevArrow: '<i class="fa-solid fa-chevron-left fa-4x"></i>',
+    nextArrow: '<i class="fa-solid fa-chevron-right fa-4x"></i>',
+  });
 
-    $('.multiple-items').slick({
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 3
-      });
+  $(".multiple-items").slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    prevArrow: '<i class="fa-solid fa-chevron-left fa-4x"></i>',
+    nextArrow: '<i class="fa-solid fa-chevron-right fa-4x"></i>',
+  });
 });
